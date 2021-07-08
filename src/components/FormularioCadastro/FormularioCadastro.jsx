875f8c24a57c0afc35c984b6820dff1671f0654d
JvPelai <jv.pelai@gmail.com>
2021-07-08 17:52:28 -0300

    material-ui + hooks

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
   return (
-    <form>
-      <label>Nome</label>
-      <input type="text" />
-      <label>Sobrenome</label>
-      <input type="text" />
-      <label>CPF</label>
-      <input type="text" />
-      <label>Promoções</label>
-      <input type="checkbox" />
-      <label>Novidades</label>
-      <input type="checkbox" />
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          let tempNome = event.target.value;
+          if (tempNome.length > 3) {
+            tempNome = tempNome.substr(0, 3);
+          }
+          setNome(tempNome);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="Sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="CPF"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" color="primary" defaultChecked />}
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" color="primary" defaultChecked />}
+      />
 
-      <button type="submit">Cadastrar</button>
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
     </form>
   );
 }
