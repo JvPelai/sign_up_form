591bfcfe9ffe0ef60348bb020100b268ec943a84
JvPelai <jv.pelai@gmail.com>
2021-07-09 17:13:02 -0300

    fix: refactor formStep rendering

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,32 +2,22 @@ import React from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
-import { Typography } from "@material-ui/core";
 import { useState } from "react";
 
 function FormularioCadastro({ validarCPF }) {
   const [currentFormStep, setCurrentFormStep] = useState(0);
+  const formSections = [
+    <DadosUsuario handleSubmit={onNextStep} />,
+    <DadosPessoais validarCPF={validarCPF} handleSubmit={onNextStep} />,
+    <DadosEntrega />,
+  ];
 
-  function onNextStep () {
-    setCurrentFormStep(currentFormStep+1);
-  }
-  function renderCurrentFormStep() {
-    switch (currentFormStep) {
-      case 0:
-        return <DadosUsuario handleSubmit={onNextStep}/>;
-      case 1:
-        return <DadosPessoais validarCPF={validarCPF} handleSubmit={onNextStep} />;
-      case 2:
-        return <DadosEntrega />;
-      default:
-        return <Typography>Erro ao carregar formulário</Typography>;
-    }
+  function onNextStep() {
+    setCurrentFormStep(currentFormStep + 1);
   }
 
-  return <>{renderCurrentFormStep()}</>;
+  return <>{formSections[currentFormStep]}</>;
 }
 
-//<DadosUsuario />
-//<DadosEntrega />
 
 export default FormularioCadastro;
